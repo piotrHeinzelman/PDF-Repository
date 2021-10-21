c7d4c396dac696e305844aa43ac936e7015401e3
Piotr Heinzelman <piotr@i-zn.pl>
2021-10-21 14:05:50 +0200

    REBOULD

--- _Java8Application_/src/com/heinzelman/MyLineComponent.java
+++ _Java8Application_/src/com/heinzelman/MyLineComponent.java
@@ -3,35 +3,65 @@ package com.heinzelman;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MyLineComponent extends JPanel {
 
-    private static final Color BG = new Color( 196,196,196 );
-    private static final Color BORDERLINE = new Color( 96,96,96 );
+    private static final Color BG_NoFile = new Color( 196,196,196 );
+    private static final Color BG_ExistsFile = new Color( 40,127,25 );
+    private static final Color BORDERLINE = new Color( 176,176,176 );
     private static final Border BORDER = BorderFactory.createLineBorder( BORDERLINE );
-    private static final Dimension DIM = new Dimension ( 200 , 50 );
-    private static final String EMPTY_TEXT = " | DROP PDF FILE HERE |";
+    private static final Dimension DIM = new Dimension ( 170 , 32 );
+    private static final String EMPTY_TEXT = " DROP PDF FILE HERE ";
 
     private static final FileDrop.Listener listener = new FileDropListener();
 
-
     private final JTextField textArea;
+    private final JButton buttonEdit;
+    private final JButton buttonDel;
+
 
     public MyLineComponent() throws HeadlessException {
-        textArea = new JTextField( ""  );
-        textArea.setBorder( BORDER );
-        textArea.setPreferredSize( DIM );
-        textArea.setBackground( BG );
+        this.textArea = new JTextField( ""  );
+        this.textArea.setBorder( BORDER );
+        this.textArea.setPreferredSize( DIM );
+        //
 
 
-        new FileDrop( textArea , listener );
+        this.buttonEdit = new JButton(" Edit ");
+        buttonEdit.addActionListener( new ButtonEditActionListener());
+
+
+        this.buttonDel = new JButton(" Delete ");
+        buttonDel.addActionListener( new ButtonDelActionListener());
 
         //ImageIcon icon = createImageIcon("images/wavy.gif",  "wavy-line border icon"); //20x22
         //JImageComponent ic = new JImageComponent(myImageGoesHere);
         //imagePanel.add(ic);
 
         this.add( textArea );
+        this.add( buttonEdit );
+        this.turnToNoFile();
     }
+
+
+    public void  turnToExistFile(){
+        this.textArea.setBackground( BG_NoFile );
+        new FileDrop( this.textArea , listener );
+    }
+
+    public void turnToNoFile(){
+        for (ActionListener al : this.textArea.getActionListeners()) {
+            System.out.println( al );
+        }
+        this.textArea.setText( EMPTY_TEXT );
+        this.textArea.setBackground( BG_NoFile );
+        new FileDrop( this.textArea , listener );
+    }
+
 }
 
 
@@ -50,3 +80,34 @@ public class MyLineComponent extends JPanel {
 
 *
 * */
+
+
+
+
+
+ class ButtonEditActionListener implements ActionListener {
+
+    private static DropTarget dropTarget;
+    private static DropTargetListener dropTargetListener;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println( e );
+    }
+
+}
+
+
+
+
+ class ButtonDelActionListener implements ActionListener {
+
+    private static DropTarget dropTarget;
+    private static DropTargetListener dropTargetListener;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println( e );
+    }
+
+}
